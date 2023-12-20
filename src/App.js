300333fdb27fb77521a460b9af579b94c71ef479
Thiago <throcha3@gmail.com>
2023-12-20 19:43:01 -0300

    feat: installed react-router | created about and home pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+//pages
+
 function App() {
   return (
     <div className="App">
-      <h1> miniblog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
