aa83c49cc8f855f09e0794eca4a2cdd81b73fe98
Thiago <throcha3@gmail.com>
2023-12-20 20:31:36 -0300

    style: create css style for navbar and footer

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -2,7 +2,7 @@ import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <h3>Write whatever you want</h3>
       <p>mini blog &copy; 2023</p>
     </footer>
