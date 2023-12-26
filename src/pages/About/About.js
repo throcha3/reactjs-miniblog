98c43f5d68a5ea8c259617f54ba4e6723646be0b
Thiago <throcha3@gmail.com>
2023-12-26 16:24:22 -0300

    feat: create content of about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        About mini <span>Blog</span>
+        <p>This project is a blog made with React.js and Firebase</p>
+      </h2>
+      <Link to="/posts/create" className="btn">
+        Create post
+      </Link>
     </div>
   );
 };
