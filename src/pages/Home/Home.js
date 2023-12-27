3746bce923973093debcff10fadc4c5319fe6808
Thiago <throcha3@gmail.com>
2023-12-27 14:01:43 -0300

    feat: show posts in Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,54 @@
+// CSS
 import styles from "./Home.module.css";
 
+// hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useNavigate, Link } from "react-router-dom";
+
+// react
+import { useState } from "react";
+
+// components
+import PostDetail from "../../components/PostDetail";
+
 const Home = () => {
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.home}>
+      <h1>See our more recent posts</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search by tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Search</button>
+      </form>
+      <div className="post-list">
+        {loading && <p>Loading...</p>}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>No posts found</p>
+            <Link to="/posts/create" className="btn">
+              Create first post
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
