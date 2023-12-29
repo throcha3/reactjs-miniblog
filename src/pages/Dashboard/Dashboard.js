b47f4fa0c383c89b55b1cf112b63eb5849c6d7bd
Thiago <throcha3@gmail.com>
2023-12-29 15:09:47 -0300

    feat: get posts from logged in user in Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,35 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+
 function Dashboard() {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //user posts
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Manage your posts</p>
+      {posts && posts.length === 0 ? (
+        <div>
+          <p>Posts not found</p>
+          <Link to="/posts/create" className="btn">
+            Create your first post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>There are posts</p>
+        </div>
+      )}
+
+      {posts && posts.map((post) => <h3>{post.title}</h3>)}
     </div>
   );
 }
